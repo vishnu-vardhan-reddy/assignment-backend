87bbd75dfd02b02625bdd99061387f8fa1aeaf72
vishnuvardhan Reddy Gajjala <vishnuvardhan4133@gmail.com>
2021-09-02 08:26:59 +0530

    add delete and update routes

--- src/v1/business_logic/product.js
+++ src/v1/business_logic/product.js
@@ -13,7 +13,36 @@ module.exports.createProduct = async (req, res) => {
     try {
         const product = new Product(req.body)
         await product.save()
-        res.send(product)
+        res.send({
+            success: true,
+            message: 'Product created',
+        })
+    } catch (error) {
+        res.send({ message: error })
+    }
+}
+
+module.exports.updateProduct = async (req, res) => {
+    const {id} = req.params
+    try {
+        const product = await Product.findByIdAndUpdate({_id: id}, req.body)
+        return res.send({
+			success: true,
+			message: 'Content updated',
+		});
+    } catch (error) {
+        res.send({ message: error })
+    }
+}
+
+module.exports.deleteProduct = async (req, res) => {
+    const {id} = req.params
+    try {
+        await Product.findByIdAndDelete({_id: id})
+        res.send({
+            success: true,
+            message: 'Content deleted',
+        });
     } catch (error) {
         res.send({ message: error })
     }
--- src/v1/routes/product.routes.js
+++ src/v1/routes/product.routes.js
@@ -1,9 +1,10 @@
-const { getAllProducts, createProduct } = require('../business_logic/product');
+const { getAllProducts, createProduct, updateProduct, deleteProduct } = require('../business_logic/product');
 
 const router = require('express').Router();
 
 router.get('/', getAllProducts);
 router.post('/', createProduct);
-
+router.put('/:id', updateProduct);
+router.delete('/:id', deleteProduct);
 
 module.exports = router;
